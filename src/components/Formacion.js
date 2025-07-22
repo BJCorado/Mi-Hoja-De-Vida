c9ab9b65d6909d64a9a5928deeda932c1e5b4dd1
BJCorado <brayncorado454@gmail.com>
2025-07-22 20:49:24 -0300

    se agrego fondo gris blur en seccion de formacion academica

--- src/components/Formacion.js
+++ src/components/Formacion.js
@@ -2,25 +2,17 @@ import React from 'react';
 
 const Formacion = () => {
   return (
-    <section className="container">
-      <h2 className="text-center mb-4 text-primary">Formación Académica</h2>
-      <div className="row justify-content-center">
-        <div className="col-md-8">
-          <ul className="list-group shadow">
-            <li className="list-group-item">
-              Técnico Operador — Academia Genesis (2017)
-            </li>
-            <li className="list-group-item">
-              Técnico Programador — Academia Genesis (2018)
-            </li>
-            <li className="list-group-item">
-              Bachillerato En Ciencias y Letras Con Orientación En Computación — IEDCO (2020-2021)
-            </li>
-          </ul>
-        </div>
+    <section className="section">
+      <div className="container text-center p-5">
+        <h2 className="mb-4 text-primary">Formación Académica</h2>
+        <p>Técnico Operador — Academia Genesis (2017)</p>
+        <p>Técnico Programador — Academia Genesis (2018)</p>
+        <p>Bachillerato En Ciencias y Letras Con Orientación En Computación — IEDCO (2020-2021)</p>
       </div>
     </section>
   );
 };
 
 export default Formacion;
+
+
