bbc15dab1c31dac5acc85de32924b346eb0fdaaf
BJCorado <brayncorado454@gmail.com>
2025-07-22 20:38:58 -0300

    se modifico el div de la seccion header para que se mostrara el fondo blur

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,14 +2,16 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <header className="text-center p-5 bg-light">
+    <header className="text-center p-5 section">
       <img src="/mi foto.jpg" alt="Foto personal" className="rounded-circle mb-3" width="150" />
       <h1>Brayan Josue Corado Robles</h1>
-      <p className="lead">Actual Estudiante de 8º Ciclo de Ingenieria En Sistemas 
-        De La Universidad Mariano Galvez De Guatemala
+      <p className="lead">
+        Actual Estudiante de 8º Ciclo de Ingeniería En Sistemas 
+        De La Universidad Mariano Gálvez De Guatemala
       </p>
     </header>
   );
 };
 
 export default Header;
+
