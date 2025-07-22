47ecf60948287a7326b0dd9b76bd5ea2abd23bea
BJCorado <brayncorado454@gmail.com>
2025-07-22 12:42:20 -0300

    agregue mi nombre completo en header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,8 @@ const Header = () => {
   return (
     <header className="text-center p-5 bg-light">
       <img src="/mi foto.jpg" alt="Foto personal" className="rounded-circle mb-3" width="150" />
-      <h1>Tu Nombre Completo</h1>
-      <p className="lead">Desarrollador Web | Estudiante | Otro</p>
+      <h1>Brayan Josue Corado Robles</h1>
+      <p className="lead">Bachiller En Ciencias y Letras Con Orientacion En Computacion | Estudiante de Ingenieria en Sistemas</p>
     </header>
   );
 };
