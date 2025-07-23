687b1c2f432a0fd5894e7c75e28e998d8e042ebc
BJCorado <brayncorado454@gmail.com>
2025-07-22 21:01:18 -0300

    se cambio el color del titulo h2

--- src/components/Formacion.js
+++ src/components/Formacion.js
@@ -4,7 +4,7 @@ const Formacion = () => {
   return (
     <section className="section">
       <div className="container text-center p-5">
-        <h2 className="mb-4 text-primary">Formación Académica</h2>
+        <h2 className="mb-4 text-white">Formación Académica</h2>
         <p>Técnico Operador — Academia Genesis (2017)</p>
         <p>Técnico Programador — Academia Genesis (2018)</p>
         <p>Bachillerato En Ciencias y Letras Con Orientación En Computación — IEDCO (2020-2021)</p>
@@ -14,5 +14,4 @@ const Formacion = () => {
 };
 
 export default Formacion;
-
 
