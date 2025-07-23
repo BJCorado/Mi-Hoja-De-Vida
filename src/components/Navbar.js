8579812df5bf0730cc6b19831513910122c0a70d
BJCorado <brayncorado454@gmail.com>
2025-07-23 16:18:03 -0300

    ajustes en nav bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { GiHamburgerMenu } from 'react-icons/gi';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 const NavigationBar = () => {
+  const [expanded, setExpanded] = useState(false);
+
   return (
     <Navbar
       expand="lg"
       fixed="top"
+      expanded={expanded}
+      onToggle={setExpanded}
       className="custom-navbar shadow"
     >
       <Container>
-        <Navbar.Brand href="#inicio" className="d-flex align-items-center gap-2 text-white">
-          <GiHamburgerMenu /> Mi Hoja De Vida
+        {/* Solo título visual, sin redirección */}
+        <Navbar.Brand className="d-flex align-items-center gap-2 text-white">
+          Mi Hoja De Vida
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
+        {/* Ícono personalizado en el botón Toggle */}
+        <Navbar.Toggle aria-controls="basic-navbar-nav">
+          <GiHamburgerMenu color="white" size={24} />
+        </Navbar.Toggle>
+
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link href="#inicio">Inicio</Nav.Link>
-            <Nav.Link href="#formacion">Formación Académica</Nav.Link>
-            <Nav.Link href="#habilidades">Habilidades Técnicas</Nav.Link>
-            <Nav.Link href="#contacto">Contacto</Nav.Link>
+            <Nav.Link href="#inicio" onClick={() => setExpanded(false)}>Inicio</Nav.Link>
+            <Nav.Link href="#formacion" onClick={() => setExpanded(false)}>Formación Académica</Nav.Link>
+            <Nav.Link href="#habilidades" onClick={() => setExpanded(false)}>Habilidades Técnicas</Nav.Link>
+            <Nav.Link href="#contacto" onClick={() => setExpanded(false)}>Contacto</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
