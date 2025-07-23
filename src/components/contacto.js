44148de13e09e2af685bf4320705cad476d5a659
BJCorado <brayncorado454@gmail.com>
2025-07-23 11:02:16 -0300

    se agrego link a usuario de git hub en la seccion de contacto

--- src/components/contacto.js
+++ src/components/contacto.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import { FaEnvelope, FaInstagram, FaFacebook } from 'react-icons/fa';
+import { FaGithub,FaEnvelope, FaInstagram, FaFacebook } from 'react-icons/fa';
 
 const Contacto = () => {
   return (
     <section className="container my-5 p-4 section">
       <h2 className="text-center mb-4">Contacto</h2>
       <div className="d-flex justify-content-center gap-4">
+                {/* GitHub */}
+        <a href="https://github.com/BJCorado" target="_blank" rel="noopener noreferrer"className="text-white fs-2"
+        >
+          <FaGithub />
+        </a>
+
         {/* Email */}
         <a href="mailto:brayancorado454@gmail.com" target="_blank" rel="noopener noreferrer" className="text-white fs-2">
           <FaEnvelope />
@@ -15,7 +21,7 @@ const Contacto = () => {
           <FaInstagram />
         </a>
         {/* Facebook */}
-        <a href="https://www.facebook.com/share/1Y46QZ9Zby/" target="_blank" rel="noopener noreferrer" className="text-white fs-2">
+        <a href="https://www.facebook.com/bjcorado" target="_blank" rel="noopener noreferrer" className="text-white fs-2">
           <FaFacebook />
         </a>
       </div>
