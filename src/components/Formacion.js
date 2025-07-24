9a20d514d41b25f4fa6b29880ade0ffc555b14c6
BJCorado <brayncorado454@gmail.com>
2025-07-24 13:24:50 -0300

    se cambio fechas

--- src/components/Formacion.js
+++ src/components/Formacion.js
@@ -5,8 +5,8 @@ const Formacion = () => {
     <section className="section">
       <div className="container text-center p-5">
         <h2 className="mb-4 text-white">Formación Académica</h2>
-        <p>Técnico Operador — Academia Genesis (2017)</p>
-        <p>Técnico Programador — Academia Genesis (2018)</p>
+        <p>Técnico Operador — Academia Genesis (2018)</p>
+        <p>Técnico Programador — Academia Genesis (2019)</p>
         <p>Bachillerato En Ciencias y Letras Con Orientación En Computación — IEDCO (2020-2021)</p>
         <p>4to Año De La Carrera De Ingenieria En Sistemas — UMG (2022-2025)</p>
       </div>
